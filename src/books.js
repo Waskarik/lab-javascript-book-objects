5d1fe95db76487c002b8d5ba3806fd7c48b7a37f
Leanderson Costa <exmoser@gmail.com>
2026-09-20 11:09:48 +0100

    done

---
+++ src/books.js
@@ -0,0 +1,102 @@
+const booksArray = [
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description:
+        "One of Hemingway's most famous works, it tells the story of Santiago...",
+    },
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description:
+        "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb...",
+    },
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description:
+        "Educated is an account of the struggle for self-invention...",
+    },
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description:
+        "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure.",
+    },
+  },
+];
+
+function getBookDetails(book) {
+  return `${book.title} - ${book.author} - ${book.pages} pages`;
+}
+
+for (const book of booksArray) {
+  delete book.details.language;
+}
+
+console.log(booksArray);
+
+for (const book of booksArray) {
+  book.readingTime = Math.ceil((book.pages * 500) / 90);
+}
+
+console.log(booksArray);
+
+const dictionary = {
+  "J. K. Rowling": [
+    ["Harry Potter and the Philosopher's Stone", 223],
+    ["Harry Potter and the Chamber of Secrets", 251],
+    ["Harry Potter and the Prisoner of Azkaban", 317],
+    ["Harry Potter and the Goblet of Fire", 636],
+  ],
+  "Neal Stephenson": [
+    ["Cryptonomicon", 928],
+    ["Anathem", 1008],
+    ["Fall; or, Dodge in Hell", 896],
+  ],
+  "Malcolm Gladwell": [
+    ["Outliers", 320],
+    ["Blink", 287],
+  ],
+};
+
+function booksByAuthor(dictionary) {
+  const books = [];
+
+  for (const author in dictionary) {
+    for (const book of dictionary[author]) {
+      books.push({
+        title: book[0],
+        pages: book[1],
+        author,
+      });
+    }
+  }
+
+  return books;
+}
+
+function averagePageCount(books) {
+  let totalPages = 0;
+
+  for (const book of books) {
+    totalPages += book.pages;
+  }
+
+  return totalPages / books.length;
+}
